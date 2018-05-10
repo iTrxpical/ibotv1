03fca87976c2b2c166756d69a0bc191429b8298a
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-10 18:29:07 +0100

    Create website.js

---
+++ commands/website.js
@@ -0,0 +1,11 @@
+var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+const db = require('quick.db')
+exports.run = (Discord, client, message, args) => {
+    
+    let thumbnail = client.user.avatarURL;
+    let name = client.user.username;
+
+        message.reply("https://is.gd/ibotonline");
+        
+
+}
