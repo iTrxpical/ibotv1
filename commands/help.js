b7c6135d4b9d18e9ce7f497bf19ed59173a537cc
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-10 18:28:22 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,16 +4,8 @@ exports.run = (Discord, client, message, args) => {
     
     let thumbnail = client.user.avatarURL;
     let name = client.user.username;
-    db.fetch(`guildPrefix_${message.guild.id}`.then(i => {
-        message.reply("Here is the help you are looking for:");
-        var embedcmds= new Discord.RichEmbed()
-            .setFooter("© Made by Aaron")
-            .setColor(randomColor)
-            .setTitle("Bot Help")
-            .setDescription("The prefix for this server is '" + i + "'. Run " + i + "cmds for info on how to use each command!")
-            .setThumbnail(thumbnail)
-            .setAuthor(name,thumbnail)
-        message.channel.sendEmbed(embedcmds);
-        message.delete(10000)
-    })
+
+        message.reply("https://is.gd/ibotonline, the prefix in this server is '!'");
+        
+
 }
