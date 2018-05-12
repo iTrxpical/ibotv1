945589c964906a2c4ee658d69b42b8b2b072f3d1
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-12 09:03:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var randomColor = Math.floor(Math.random() * 16777215).toString(16);
    
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! There are no apparent major bugs.`);
-    client.user.setActivity(' ' + client.user.tag + ' | !help', { type: 'PLAYING' });
+    client.user.setActivity('on ' + client.guilds.size + ' servers! | !help', { type: 'WATCHING' });
     client.user.setStatus("online");
 });
 
