68f0ae53ee4af8e2acf24200de52c994db58b357
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-12 21:26:11 +0100

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,3 +1,4 @@
 exports.run = (Discord, client, message, args) => {
-message.channel.send(" Users: " + client.users.size.toLocaleString() + " Guilds: " + client.guilds.size)
+var list = myClient.channels.array().sort();
+message.channel.send(" Users: " + client.users.size.toLocaleString() + " Guilds: " + client.guilds.size\n " + list)
 }
