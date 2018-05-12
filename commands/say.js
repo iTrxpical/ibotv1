40eea0694b78a9fdb1603fd21d8bebc8522a80fe
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-12 09:18:36 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,17 +2,31 @@ exports.run = (Discord, client, message, args) => {
 
 let sender = message.author;
 let allowedRole = message.guild.roles.find("name", "bot admin");
+let sendchannel = message.channel;
 
-       if(message.member.roles.has(allowedRole.id)) {
+       if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("wot u doin? this command is for kewl ppl only (If you think this is wrong, make sure you have the administrator discord permission)")
+
         const sayMessage = args.join(" ");
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
     message.channel.send(sayMessage);
+       let reason = args.slice(0).join(" ");
+       let staffc = message.guild.channels.find("name", "logs")
         
-       } else {
-         message.channel.send("The 'bot admin' discord role is required to run this command.")
-         message.delete()
-         .then(m=>m.delete(10000))
-       }
+        if (!staffc) 
+            return message.reply('I cannot find a logs channel');
+
+        if (reason.length < 1) 
+            return message.reply('You must supply something to say.');
+
+
+        const embed = new Discord.RichEmbed()
+            .setColor(0x8cff00)
+            .setTimestamp()
+            .setDescription(`**Action:** Say\n**User:** ${message.author.tag}\n**They said:** ${reason}\n**In Channel:** ${sendchannel}`);
+        staffc.send({embed});
+
+        
+        
 }
