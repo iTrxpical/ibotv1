bb760856c7bfbab1ded13f85ebdf34e5a44d8527
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-12 22:25:22 +0100

    Update backdoor.js

--- commands/backdoor.js
+++ commands/backdoor.js
@@ -1,6 +1,7 @@
 
 exports.run = (Discord, client, message, args) => {
 let reason = args.slice(0).join(" ");
+let firstChannel = reason.channels.filter(c => c.type === "text").first();
 client.channels.get(reason).createInvite().then(invite =>
     message.channel.send(invite.url));
 }
