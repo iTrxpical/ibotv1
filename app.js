cd570612d1a57016fbeaa69ac0613bbb76958cde
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-12 22:21:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ var randomColor = Math.floor(Math.random() * 16777215).toString(16);
    
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! There are no apparent major bugs.`);
-    client.user.setActivity('with ' + client.users.size.toLocaleString() + ' users! | ibot.space', { type: 'WATCHING' });
     client.user.setStatus("online");
 });
 
 client.on('ready', () => {
     setInterval(() => {
         dbl.postStats(client.guilds.size);
+        client.user.setActivity('with ' + client.users.size.toLocaleString() + ' users! | ibot.space', { type: 'WATCHING' });
     }, 1800000);
 });
 
