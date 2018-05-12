6fbfbba3c3a2604d03a70c6320664d9f7a0865db
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-12 08:47:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const db = require('quick.db');
 const { get } = require("snekfetch"); 
 const superagent = require("superagent");
 const weather = require('weather-js');
-const DBL = require("dblapi-js");
+const DBL = require("dblapi");
 const dbl = new DBL('Your discordbots.org token', client);
 
 var randomColor = Math.floor(Math.random() * 16777215).toString(16);
