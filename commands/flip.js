b5d02d842f24accec581775b8810a63647610e08
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-17 16:42:45 +0100

    Update flip.js

--- commands/flip.js
+++ commands/flip.js
@@ -1,7 +1,16 @@
-var flip = require("flip-text");
+const mapping = '¡"#$%⅋,)(*+\'-˙/0ƖᄅƐㄣϛ9ㄥ86:;<=>¿@∀qƆpƎℲפHIſʞ˥WNOԀQɹS┴∩ΛMX⅄Z[/]^_`ɐqɔpǝɟƃɥᴉɾʞlɯuodbɹsʇnʌʍxʎz{|}~';
+// Start with the character '!'
+const OFFSET = '!'.charCodeAt(0);
 
+exports.run = async (Discord, client, message, args, level) => { // eslint-disable-line no-unused-vars
+  if (args.length < 1) {
+    message.channel.send('You must provide text to flip.');
+}
 
-exports.run = (Discord, client, message, args) => {
-	if(args) return message.channel.send(flip(args));
-	else return false;
+message.channel.send(
+    args.join(' ').split('')
+        .map(c => c.charCodeAt(0) - OFFSET)
+        .map(c => mapping[c] || ' ')
+        .reverse().join('')
+);
 };
