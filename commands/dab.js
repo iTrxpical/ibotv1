46201f9882839028f634323636061f5e8d05d67f
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-17 18:03:49 +0100

    Update dab.js

--- commands/dab.js
+++ commands/dab.js
@@ -1,7 +1,7 @@
 
 exports.run = (Discord, client, message, args) => {
   
-  let dogembed = new Discord.RickEmbed()
+  let dogembed = new Discord.RichEmbed()
   .setColor("#ff9900")
   .setTitle("Dab")
   .setImage('https://media.giphy.com/media/x4gQEhf736ZMc/giphy.gif');
