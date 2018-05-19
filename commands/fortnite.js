34be705a8f9681071eed7a8127e237119a93eae0
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-19 13:06:44 +0100

    Update fortnite.js

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -1,39 +1,34 @@
+const Discord = require("discord.js");
+const apikey = "948ac340-6e14-4d31-9a8c-49b01fa64178";
+const Fortnite = require("fortnite");
+const ft = new Fortnite(apikey.fortnite);
 
-const Fortnite = require('fortnite');
-const stats = new Fortnite("948ac340-6e14-4d31-9a8c-49b01fa64178");
-const Discord = require('discord.js');
-
-module.exports.run = async (client, message, args, tools) => {
-
-        let platform;
-        let username;
-
-        if (!['pc', 'xbl', 'psn'].includes(args[0])) return message.channel.send('**Please Include the platform: `!fortnite [ pc | xbl | psn ] <username>`**');
-        if (!args[1]) return message.channel.send('**Please Include the username: `!fortnite [ pc | xbl | psn ] <username>`**');
-
-        platform = args.shift();
-        username = args.join(' ');
-
-        stats.getInfo(username, platform).then(data => {
-                    const embed = new Discord.MessageEmbed()
-                        .setColor(0xffffff)
-                        .setTitle(`Stats for ${data.username}`)
-                        .setDescription(`**Top Placement**\n\n**Top 3s:** *${data.lifetimeStats[0].value}*\n**Top 5s:** *${data.lifetimeStats[1].value}*\n**Top 6s:** *${data.lifetimeStats[3].value}*\n**Top 12s:** *${data.lifetimeStats[4].value}*\n**Top 25s:** *${data.lifetimeStats[5].value}*`, true)
-                        .addField('Total Score', data.lifetimeStats[6].value, true)
-                        .addField('Matches Played', data.lifetimeStats[7].value, true)
-                        .addField('Wins', data.lifetimeStats[8].value, true)
-                        .addField('Win Percentage', data.lifetimeStats[9].value, true)
-                        .addField('Kills', data.lifetimeStats[10].value, true)
-                        .addField('K/D Ratio', data.lifetimeStats[11].value, true)
-                        .addField('Kills Per Minute', data.lifetimeStats[12].value, true)
-                        .addField('Time Played', data.lifetimeStats[13].value, true)
-                        .addField('Average Survival Time', data.lifetimeStats[14].value, true)
-
-                    message.channel.send(embed)
-                        .catch(error => {
-
-                            message.channel.send('Username not found!');
-
-                        })
-
-                }
+module.exports.run = async (client, message, args) => {
+        message.delete();
+        //
+        let username = args[0];
+        let platform = args[1] || "pc";
+        
+        let data = ft.getInfo(username, platform).then(data => {
+                
+                let stats = data.lifetimeStatus;
+                let kills = stats.find(s => s.stat == 'kills');
+                let wins = stats.find(s => s.stat == 'wins');
+                let kd = stats.find(s => s.stat == 'kd');
+                let mPlayed = stats.find(s => s.stat == 'matchesPlayed');
+                let tPlayed = stats.find(s => s.stat == 'timePlayed');
+                let asTime = stats.find(s => s.stat == 'avgSurvivalTime');
+                
+                let embed = new Discord.RichEmbed()
+                .setTitle("Fortnite Stats")
+                .setAuthor(data.username)
+                .addField("Kills", kills.value, true)
+                
+                message.channel.send(embed);
+        }).catch(e => {
+                console.log(e);
+                message.channel.send("Couldn't find that username in the database");
+        });
+        
+        
+                
