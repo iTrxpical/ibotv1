810b3bf4f336a5e1b8b2f4dbe15f3ad5e6939a7c
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-19 13:31:19 +0100

    Update fortnite.js

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -9,25 +9,7 @@ module.exports.run = async (client, message, args) => {
         let platform = args[1] || "pc";
         
         fortnite.user(username, platform).then(console.log);
-                
-                let stats = data.lifetimeStatus;
-                let kills = stats.find(s => s.stat == 'kills');
-                let wins = stats.find(s => s.stat == 'wins');
-                let kd = stats.find(s => s.stat == 'kd');
-                let mPlayed = stats.find(s => s.stat == 'matchesPlayed');
-                let tPlayed = stats.find(s => s.stat == 'timePlayed');
-                let asTime = stats.find(s => s.stat == 'avgSurvivalTime');
-                
-                let embed = new Discord.RichEmbed()
-                .setTitle("Fortnite Stats")
-                .setAuthor(data.username)
-                .addField("Kills", kills.value, true)
-                
-                message.channel.send(embed);
-        }).catch(e => {
-                console.log(e);
-                message.channel.send("Couldn't find that username in the database");
-        });
+               
 }
         
         
