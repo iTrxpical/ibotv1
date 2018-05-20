fd9b8b81aa80bbf4e64bd582af7a3d56caaf5eb6
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-20 19:23:25 +0100

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -1,29 +1,21 @@
-const Discord = require('discord.js'),
-  	  db = require('quick.db');
+
+const Discord = require('discord.js')
+const db = require('quick.db');
 
 exports.run = async (client, message, args, tools) => {
 
-  // Create a new 'AFK' table w/ Quick.db
   const status = new db.table('AFKs');
-
-  // Fetch user object from that table
   let afk = await status.fetch(message.author.id);
+  const embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
 
-  // Form Embed
-  const embed = new Discord.MessageEmbed()
-    .setColor(0xffffff)
-
-  if (!afk) { // Run if they are NOT afk, or afk is null
-    embed.setFooter('You are now AFK.'); // Modify Embed
-    // Add the user to the AFK pool
+  if (!afk) { 
+    embed.setDescription('You are now AFK.');
     status.set(message.author.id, args.join(' ') || `Sorry, ${message.author.username} is AFK.`);
-  } else { // Run if they ARE afk
-    embed.setFooter('You are no longer AFK.'); // Modify Embed
-    // Remove the user from the AFK pool
+  } else { 
+    embed.setDescription('You are no longer AFK.'); 
     status.delete(message.author.id);
+      
   }
-
-  // Send Embed
   message.channel.send(embed);
-
 }
