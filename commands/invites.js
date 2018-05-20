0f05cfc0f424a148c3eb1c3c862143f1c3a551d1
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-20 11:19:20 +0100

    Update invites.js

--- commands/invites.js
+++ commands/invites.js
@@ -1,42 +1,29 @@
- // This episode will go over Invite Leaderboards, sorted.
-const Discord = require('discord.js'); // This will be used for creating embed
-const arraySort = require('array-sort'); // This will be used for sorting arrays
-const table = require('table'); // This will be used for preparing the output to a table
-const send = require('quick.hook'); // This will be used for creating & sending webhooks
-const client = new Discord.Client();
+const Discord = require('discord.js'),
+     arraySort = require('array-sort'),
+     table = require('table'),
+     send = require("quick.hook");
 
-// We can call our command handler here
-exports.run = async (Discord, message, args, tools, client) => {
-    // Be sure to call this in async, as we will be fetching the invites of the guild
+exports.run = async (client,message,args,tools) => {
 
-    // First, we need to fetch the invites
-    let invites = await message.guild.fetchInvites().catch(error => { // This will store all of the invites into the variable
-        // If an error is catched, it will run this...
-        return message.channel.send('Sorry, I don\'t have the proper permissions to view invites!');
-    }) // This will store all of the invites into the variable
+let invites = await message.guild.fetchInvites().catch(error =>{
+return tools.embed(message.channel, "Sorry I don't Have Perm To View Invites :x:" , 5000)
+})
 
-    // Next, we can turn invites into an array
     invites = invites.array();
 
-    // Now, using arraySort, we can sort the array by 'uses'
-    arraySort(invites, 'uses', { reverse: true }); // Be sure to enable 'reverse'
+    arraySort(invites, 'uses', { reverse: true });
 
-    // Next, we need to go through each invite on the server, to format it for a table
-    let possibleInvites = [['User', 'Uses']]; // Each array object is a rown in the array, we can start up by setting the header as 'User' & 'Uses'
+    let possibleInvites = [['User', 'Uses']];
     invites.forEach(function(invite) {
-        possibleInvites.push([invite.inviter.username, invite.uses]); // This will then push 2 items into another row
+      possibleInvites.push([invite.inviter.username , invite.uses]);
     })
+    const embed = new Discord.RichEmbed()
+    .setColor(0x7289da)
+    .setTitle("Server Invites")
+    .addField('Leaderboard' , `\`\`\`${table.table(possibleInvites)}\`\`\``);
 
-    // Create the output embed
-    const embed = new Discord.MessageEmbed()
-        .setColor(0xCB5A5E)
-        .addField('Leaderboard', `\`\`\`${table.table(possibleInvites)}\`\`\``); // This will be the field holding the leaderboard
-        // Be sure to put the table in a codeblock for proper formatting
+  send( message.channel,embed,{
+      name: "leaderboard"
+  })
 
-    // Now, we can send the embed to chat - Instead of a regular message, we can use quick.hook
-    send(message.channel, embed, {
-        name: 'Server Invites',
-        icon: 'https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/trophy-128.png'
-    })
-    
 }
