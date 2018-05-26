f25b1dce738a7ff472956393ecab0e765c418e54
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-26 14:05:18 +0100

    Create rank.js

---
+++ commands/rank.js
@@ -0,0 +1,35 @@
+exports.run = (Discord, client, message, args) => {
+
+roblox.login({username: "Username_Here", password: "Password_Here"}).then((success) => {
+
+}).catch(() => {console.log("Failed to login.");});
+
+    	var username = args[1]
+    	if (username){
+    		message.channel.send(`Checking ROBLOX for ${username}`)
+    		roblox.getIdFromUsername(username)
+			.then(function(id){
+				roblox.getRankInGroup(groupId, id)
+				.then(function(rank){
+					if(maximumRank <= rank){
+						message.channel.send(`${id} is rank ${rank} and not promotable.`)
+					} else {
+						message.channel.send(`${id} is rank ${rank} and promotable.`)
+						roblox.promote(groupId, id)
+						.then(function(roles){
+							message.channel.send(`Promoted from ${roles.oldRole.Name} to ${roles.newRole.Name}`)
+						}).catch(function(err){
+							message.channel.send("Failed to promote.")
+						});
+					}
+				}).catch(function(err){
+					message.channel.send("Couldn't get him in the group.")
+				});
+			}).catch(function(err){ 
+				message.channel.send(`Sorry, but ${username} doesn't exist on ROBLOX.`)
+			});
+    	} else {
+    		message.channel.send("Please enter a username.")
+    	}
+    	return;
+}
