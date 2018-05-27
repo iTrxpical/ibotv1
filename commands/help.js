44f51de297f1d7e04909b86bd7b429673fa7f82d
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-27 17:29:10 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,11 @@
 var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 const db = require('quick.db')
 exports.run = (Discord, client, message, args) => {
-    
+    const ayy = client.emojis.find("name", "tickYes");
     let thumbnail = client.user.avatarURL;
     let name = client.user.username;
 
-        message.reply("http://ibot.space/, the prefix in this server is '!'");
+        message.reply(ayy + "You can find a list of commands at https://ibot.space/.");
         
 
 }
