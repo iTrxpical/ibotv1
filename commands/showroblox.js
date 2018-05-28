328e97d7b5995320d9d159eb1df5b4e5c05fa7b1
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-28 21:27:34 +0100

    Update showroblox.js

--- commands/showroblox.js
+++ commands/showroblox.js
@@ -5,5 +5,5 @@ let guildid = message.guild.id;
 let username = stored.username;
 let groupid = stored.groupid;
 let maxrank = stored.maxrank;
-message.channel.send("Roblox infomation: Account set to " + username + " with password " + password + " on group " + groupid + " with maxrank " + maxrank + ".")
+message.channel.send("Roblox infomation: Account set to " + username + " on group " + groupid + " with maxrank " + maxrank + ".")
 }
