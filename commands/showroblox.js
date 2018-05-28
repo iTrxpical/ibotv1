f0a8bd5e8d879530698d1990179b8f307554119a
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-28 21:30:40 +0100

    Update showroblox.js

--- commands/showroblox.js
+++ commands/showroblox.js
@@ -1,5 +1,5 @@
 const db = require('quick.db')
 exports.run = (Discord, client, message, args) => {
 let stored = db.fetch(`RobloxProfile_${message.guild.id}`)
-message.channel.send(stored)
+message.channel.send(stored.username)
 }
