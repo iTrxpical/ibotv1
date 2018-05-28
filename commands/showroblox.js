bb0ace51fd5b26f2f209d25a49fa9a8c5fea6d57
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-28 21:29:24 +0100

    Update showroblox.js

--- commands/showroblox.js
+++ commands/showroblox.js
@@ -1,9 +1,5 @@
 const db = require('quick.db')
 exports.run = (Discord, client, message, args) => {
 let stored = db.fetch(`RobloxProfile_${message.guild.id}`)
-let guildid = message.guild.id;
-let username = stored.username;
-let groupid = stored.groupid;
-let maxrank = stored.maxrank;
-message.channel.send("Roblox infomation: Account set to " + username + " on group " + groupid + " with maxrank " + maxrank + ".")
+message.channel.send(stored)
 }
