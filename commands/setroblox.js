94c50f7e13fcf83a62790fc9eb59d0cbedb2ac7c
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-29 18:40:35 +0100

    Update setroblox.js

--- commands/setroblox.js
+++ commands/setroblox.js
@@ -5,6 +5,7 @@ let username = args[0];
 let password = args[1];
 let groupid = args[2];
 let maxrank = args[3];
+if (!maxrank) return message.reply("Looks like you've used this command wrong. Here's the setroblox command useage:\n !setroblox RobloxUsername RobloxPassword RobloxGroupID MaxRank, we suggest you do this in a private channel")
 if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("No can do pal!, ADMINISTRATOR is needed.");
 db.set(`RobloxProfile_${message.guild.id}`, { username: args[0], password: args[1], guildid: message.guild.id, groupid: args[2], maxrank: args[3] })
 message.channel.send("Roblox infomation updated. Account set to " + username + " with password " + password + " on group " + groupid + " with maxrank " + maxrank + ".")
